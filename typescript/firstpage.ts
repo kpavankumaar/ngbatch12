67e08403c841a6f5c6ce5ef17037009acd5f7820
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-07-29 09:15:20 +0530

    const let declaration

--- typescript/firstpage.ts
+++ typescript/firstpage.ts
@@ -1,8 +1,32 @@
 let name_1 = "ravi";
 console.log(name_1);
-for(var i = 0 ; i < 10 ; i = i + 1 ){
+// function printOut(i){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000)
+// }
+// for(var i = 0 ; i < 10 ; i = i + 1 ){
+//    printOut(i);
+// }
+// function printOut(i){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000)
+// }
+for(let i = 0 ; i < 10 ; i = i + 1 ){
     setTimeout(function(){
         console.log(i);
     }, 1000)
+};
+var divEl = document.getElementsByTagName('div')[0]
+var data = ['ravi','krishna'];
+divEl.onclick = run(data);
+function run(data){
+    return function(e){
+        console.log("name:",data[0], e);
+    }
     
 }
+const num = 10;
+num = 10;
+console.log(num);
