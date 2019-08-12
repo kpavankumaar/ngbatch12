aef5cde7ea3582afb7bb0a88ebb92cd170d79bee
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-08-12 09:02:07 +0530

    one way binding

--- angular/basics/src/app/app.component.ts
+++ angular/basics/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Welcome to Angular Session';
+  @ViewChild('user') userElement:ElementRef;
+  ngOnInit(){
+    console.log(this.userElement.nativeElement.value)
+  }
+  color = 'grey';
 }
