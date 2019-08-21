df93607fba94155a100b8a40362bdf44efceef73
unknown <https://github.com/sujithreddy9493/suij.git>
2019-08-21 09:26:26 +0530

    directives , two way binding

--- angular/basics/src/app/app.component.ts
+++ angular/basics/src/app/app.component.ts
@@ -7,9 +7,24 @@ import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Welcome to Angular Session';
+  username:string;
+  arr = ['red','green','blue'];
   @ViewChild('user') userElement:ElementRef;
   ngOnInit(){
     console.log(this.userElement.nativeElement.value)
   }
   color = 'grey';
+  changeColor(){
+    console.log("data");
+  }
+  updateUserDetails(e){
+    this.username = e ;
+  }
+  changeInUserDetails(e){
+    this.username = e;
+  }
+  detectChange(e){
+    this.username = e;
+    console.log(e)
+  }
 }
