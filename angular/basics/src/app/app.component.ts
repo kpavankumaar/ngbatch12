894a94682ad5318ac2ddd4e768f3439f7cf53452
unknown <k.pavankumaar@gmail.com>
2019-08-22 08:19:44 +0530

    one way binding

--- angular/basics/src/app/app.component.ts
+++ angular/basics/src/app/app.component.ts
@@ -7,9 +7,20 @@ import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Welcome to Angular Session';
+  username;
   @ViewChild('user') userElement:ElementRef;
   ngOnInit(){
     console.log(this.userElement.nativeElement.value)
   }
   color = 'grey';
+  changeColor(e){
+    
+    this.color = this.color == "green" ? "grey" : "green";
+    
+    console.log('change color', this.color, e);
+  }
+  updateUsername(e){
+    console.log('update username ',e);
+    this.username = e;
+  }
 }
