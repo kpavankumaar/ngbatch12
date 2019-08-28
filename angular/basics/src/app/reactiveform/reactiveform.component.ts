37f7693aa96a0f8291b4fb7c381ee0b1091d2d42
unknown <k.pavankumaar@gmail.com>
2019-08-28 09:32:45 +0530

    reactiveforms

--- angular/basics/src/app/reactiveform/reactiveform.component.ts
+++ angular/basics/src/app/reactiveform/reactiveform.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+function ratingValidation(a,b){
+  return function (control:AbstractControl):null | {[key:string]:boolean}{
+    console.log(control.value)
+    if(control.value < a || control.value > b){
+      return {
+        check:true
+      }
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-reactiveform',
@@ -8,8 +20,12 @@ import { Component, OnInit } from '@angular/core';
 export class ReactiveformComponent implements OnInit {
 
   constructor() { }
-
+  rootFormGroup;
   ngOnInit() {
+    this.rootFormGroup = new FormGroup({
+      name: new FormControl('krishna',Validators.required),
+      rating:new FormControl('',[Validators.required,ratingValidation(1,10)]),
+    })
   }
 
 }
