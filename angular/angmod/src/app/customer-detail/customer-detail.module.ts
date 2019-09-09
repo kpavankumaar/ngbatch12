4844efad399e37be54576803fda27350411d3f45
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-09-09 09:57:26 +0530

    customersModule

---
+++ angular/angmod/src/app/customer-detail/customer-detail.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerDetailComponent } from './customer-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CustomerDetailComponent]
+})
+export class CustomerDetailModule { }
--- angular/basics/src/app/app.module.ts
+++ angular/basics/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { TemlateDrivenComponent } from './temlate-driven/temlate-driven.component';
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
-
+// const route:Routes = [
+//   {path:"customers",component:CustomersComponent},
+//   {path:"detail",component:CustomerDetailComponent},
+// ]
+const route:Routes = [
+  { 
+    path:"customers",
+    component:CustomersComponent,
+    children:[
+      {path:'detail', component:CustomerDetailComponent}
+    ]
+  },
+  
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +33,7 @@ import { CustomerDetailComponent } from './customer-detail/customer-detail.compo
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(route),
     ReactiveFormsModule
   ],
   providers: [],
