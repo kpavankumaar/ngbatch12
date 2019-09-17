c0083bfff465d3df58cf7f44b5322a60a2af10c4
unknown <k.pavankumaar@gmail.com>
2019-09-17 10:05:14 +0530

    services

--- angular/customersmanager/src/app/customers/customerscardview.component.ts
+++ angular/customersmanager/src/app/customers/customerscardview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../core/services/data.service';
 
 @Component({
   selector: 'app-customerscardview',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerscardviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getCustomersData().subscribe()
   }
 
 }
